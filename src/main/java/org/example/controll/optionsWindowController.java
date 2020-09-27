07f1bcb9cc7e0f45fe3228d2b5c62fbc314422cb
Marek Frańczak <01101082@pw.edu.pl>
2020-09-27 17:35:26 +0200

    add optin window controller

---
+++ src/main/java/org/example/controll/optionsWindowController.java
@@ -0,0 +1,26 @@
+package org.example.controll;
+
+import javafx.fxml.FXML;
+
+public class optionsWindowController {
+
+    @FXML
+    private void edit(){
+
+    }
+
+    @FXML
+    private void saveChanges(){
+
+    }
+
+    @FXML
+    private void openGuide(){
+
+    }
+
+    @FXML
+    private void openInfoWindow(){
+
+    }
+}
